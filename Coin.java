7b145d6c674b3ee20acf101419c3c1062f5466a5
Yes1001ok <yes1001ok@126.com>
2017-05-28 16:59:42 +0800

    add 2 files:Coin.java, Database.java

---
+++ Coin.java
@@ -0,0 +1,39 @@
+package bbbb;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Coin {
+	private HashMap<Integer,String> coinnames =new  HashMap<Integer,String>();
+	
+	public Coin(){
+		coinnames.put(1, "penny");
+		coinnames.put(10, "dime");
+		coinnames.put(25, "quarter");
+		coinnames.put(50, "half-dollar");
+		coinnames.put(50, "5ë");
+		System.out.println(coinnames.keySet().size());
+		System.out.println(coinnames);
+		for(Integer k: coinnames.keySet()){
+//			String s = coinnames.get(k);
+//			System.out.println(s);
+			System.out.println(coinnames.get(k));
+		}
+	}
+	public String getName(int amount){
+		if(coinnames.containsKey(amount))
+			return coinnames.get(amount);
+		else
+			return "NOT FOUND";
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int amount = scan.nextInt();
+		Coin coin = new Coin();
+		String name = coin.getName(amount);
+		System.out.println(name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
